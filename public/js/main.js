e97095a7a732b3304612b185743bb6ecbe1fca01
rc1021 <rc1021@gmail.com>
2022-05-31 17:00:27 +0800

    更新 click

--- public/js/main.js
+++ public/js/main.js
@@ -59,7 +59,7 @@ $(function() {
     $(".theNavibar").attr("data-status", "off");
     return false;
   });
-  $(".g9, .gosize").click(function() {
+  $(document).on('click', ".g9, .gosize", function() {
     $("body, html").stop(true,false).animate({
       scrollTop: $(".sec9").offset().top - naviH
     }, 100,"easeOutQuart");
